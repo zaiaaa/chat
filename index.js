332032a23c761b896caae4665774465fac0427e5
zaiaaa <gustavo.0801214@gmail.com>
2023-11-07 18:25:36 -0300

    did function refreshMessages and the socket

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+
+const { Server } = require('socket.io')
+
 const { createServer } = require('http')
 const port = 3000
-const server = createServer(app)
 const { join } = require('node:path') 
+const server = createServer(app)
+const io = new Server(server)
+
+const controller = require('./controller')
+
+const connection = require('./inc/connection')
+const table = require('./inc/table')
+
+table.init(connection)
+
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'index.html'))  
@@ -13,12 +25,35 @@ app.get('/chat.html', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'chat.html'))  
 })
 
+io.on('connection', (socket) => {    
+    function refreshMessages(){
+        controller.list().then((response) => {
+            socket.emit('messages', response)
+        })
+    }
+    refreshMessages()
+    
+    
+    socket.on('username_conn', (user) => {
+        console.log(`user ${user} Online`)
+    })
+    socket.on('sendMessage', (data) => {
+        const sql = `INSERT INTO message(content, user, sendDate) VALUES('${data.content}', '${data.user}', '${data.sendDate}')`
+        connection.query(sql, (e) => {
+            if(e){
+                console.log('include error -> ', e.message)
+                return
+            }
+            console.log('inclusao feita')
+            refreshMessages()
+        })
+    })
+})
+
 
 server.listen(port, () => {
     console.log('rodando na porta 3000')
 })
 
-const connection = require('./inc/connection')
-const table = require('./inc/table')
 
-table.init(connection)
+
