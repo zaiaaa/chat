2a6832d9c4d65f6e24ccb238f039e4dcff7f9dab
zaiaaa <gustavo.0801214@gmail.com>
2023-11-07 20:59:05 -0300

    finish

--- index.js
+++ index.js
@@ -50,9 +50,6 @@ io.on('connection', (socket) => {
 })
 
 
-server.listen(proccess.env.PORT ?? 3000, () => {
+server.listen(process.env.PORT ?? 3000, () => {
     console.log('rodando na porta 3000')
 })
-
-
-
